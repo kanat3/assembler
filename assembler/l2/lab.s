dbae48f4380ad2aa9f2a643a64f89d830b0c2b4a
kanat3 <30739653+kanat3@users.noreply.github.com>
2022-05-20 00:45:06 +0300

    change tabs

--- assembler/l2/lab.s
+++ assembler/l2/lab.s
@@ -13,7 +13,7 @@ matrix:
 	.quad	0, 8, 3, -1, -1, 0
     .quad   0, 3, -33, -4, 5, 0
 maxs:
-    .skip 40 // m*8-bit
+	.skip 40 // m*8-bit
 	.text
 	.align	2
 	.global _start
@@ -51,52 +51,52 @@ L2:
 L3:
 	sub	x4, x1, #1
 	mov	x5, #0 // sorted - 1, 0 - no
-    mov x11, #1 // index
-    mov x12, #1
+	mov x11, #1 // index
+	mov x12, #1
 L4:
-    mov x6, x11
-    mov x7, x11
-    cmp x5, #1
-    beq L9 // array is sorted
-    ldr x9, [x3, x6, lsl #3]
-    b L5
+	mov x6, x11
+	mov x7, x11
+	cmp x5, #1
+	beq L9 // array is sorted
+	ldr x9, [x3, x6, lsl #3]
+	b L5
 0:
-    mov x11, #1
-    mov x5, x12
-    mov x12, #1
-    b L4
+	mov x11, #1
+	mov x5, x12
+	mov x12, #1
+	b L4
 check:
-    tst x11, #1
-    beq 0b
-    mov x11, #0
-    mov x6, x11
-    mov x7, x11
-    ldr x9, [x3, x6, lsl #3]
+	tst x11, #1
+	beq 0b
+	mov x11, #0
+	mov x6, x11
+	mov x7, x11
+	ldr x9, [x3, x6, lsl #3]
 L5:
 	add	x6, x6, #1
 	cmp	x6, x4
-    bgt check
-    mov x5, #1
+	bgt check
+	mov x5, #1
 	ldr	x8, [x3, x6, lsl #3]
 	cmp	x8, x9
 .ifdef sort
 	bge	0f
 .endif
 .ifdef rsort
-    ble 0f
+	ble 0f
 .endif
 	mov	x7, x6
 	mov	x9, x8
-    mov x5, #0 // yes swap
-    mov x12, #0 // swap in iteration
-    b L6
+	mov x5, #0 // yes swap
+	mov x12, #0 // swap in iteration
+	b L6
 0:
-    add x11, x11, #2
-    mov x6, x11
-    mov x7, x11
-    mov x5, x12
-    ldr x9, [x3, x6, lsl #3]
-    b L5
+	add x11, x11, #2
+	mov x6, x11
+	mov x7, x11
+	mov x5, x12
+	ldr x9, [x3, x6, lsl #3]
+	b L5
 L6:
 	ldr	x8, [x3, x11, lsl #3]
 	str	x8, [x3, x7, lsl #3]
@@ -116,8 +116,8 @@ L7:
 	add	x10, x10, #1
 	b	L7
 L8:
-    add x11, x11, #2
-    mov x5, x12
+	add x11, x11, #2
+	mov x5, x12
 	b	L4
 L9:
 	mov	x0, #0
