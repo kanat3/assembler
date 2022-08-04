f7a191b7bf1d3f33cd1f685767fb1503cd0566ea
kanat3 <30739653+kanat3@users.noreply.github.com>
2022-08-05 01:11:14 +0300

    Update lab.s

--- assembler/ll2/lab.s
+++ assembler/ll2/lab.s
@@ -1,7 +1,6 @@
 	.arch armv8-a
-//	Sorting columns of matrix by min elements
-//	Try Shell
-//
+	//	Sorting columns of matrix by min elements
+	//	Shell
 	.data
 	.align	3
 n:
@@ -20,47 +19,44 @@ maxs:
 	.global _start
 	.type	_start, %function
 _start:
-	adr	x2, n
-	ldr	w0, [x2]
-	adr	x2, m
-	ldr	w1, [x2]
-	adr	x2, matrix
-	adr	x3, maxs
-	mov	x4, #0
+	adr		x2, n
+	ldr		w0, [x2]
+	adr		x2, m
+	ldr		w1, [x2]
+	adr		x2, matrix
+	adr		x3, maxs
+	mov		x4, #0
 L0:
-	cmp	x4, x1
-	bge	L3
-	mov	x5, #0
-	lsl	x6, x4, #3
-	ldr	x7, [x2, x6]
-	add	x6, x6, x1, lsl #3
-	add	x5, x5, #1
+	cmp		x4, x1
+	bge		L3
+	mov		x5, #0
+	lsl		x6, x4, #3
+	ldr		x7, [x2, x6]
+	add		x6, x6, x1, lsl #3
+	add		x5, x5, #1
 L1:
-	cmp	x5, x0
-	bge	L2
-	ldr	x8, [x2, x6]
-	add	x6, x6, x1, lsl #3
-	add	x5, x5, #1
-	cmp	x7, x8
-	ble	L1
-	mov	x7, x8
-	b	L1
+	cmp		x5, x0
+	bge		L2
+	ldr		x8, [x2, x6]
+	add		x6, x6, x1, lsl #3
+	add		x5, x5, #1
+	cmp		x7, x8
+	ble		L1
+	mov		x7, x8
+	b		L1
 L2:
-	str	x7, [x3, x4, lsl #3]
-	add	x4, x4, #1
-	b	L0
+	str		x7, [x3, x4, lsl #3]
+	add		x4, x4, #1
+	b		L0
 L3:
-	mov	x5, #0
-//*****************************//
-// array length m
-// x3 busy
-	mov x6, #2
-	b L4
+	mov		x5, #0
+	mov 		x6, #2
+	b 		L4
 L4:
 	// x4 = gap
-	udiv	x4, x4, x6
+	udiv		x4, x4, x6
 	cmp		x4, #0
-	ble		end // <=
+	ble		end
 	mov		x7, x4 // i = gap
 	sub		x7, x7, #1
 L5:
@@ -68,12 +64,11 @@ L5:
 	adr		x1, m
 	ldr		w1, [x1] // load m
 	cmp		x7, x1
-	bge		L4 // >=
-	//b 		L6
+	bge		L4
 	sub		x8, x7, x4 //j = i - gap
 L6:
 	cmp		x8, #0
-	blt		L5 // <
+	blt		L5
 	add		x9, x8, x4 // j + gap
 	ldr		x1, [x3, x8, lsl #3] // arr[j]
 	ldr		x2, [x3, x9, lsl #3] // arr[j+gap]
@@ -84,34 +79,32 @@ L6:
 .ifdef rsort
 	bge		L5
 .endif
-	// j+gap in x9 | j in x8
-	str 	x2, [x3, x8, lsl #3] // arr[j] = arr[j + gap]
-	str 	x1, [x3, x9, lsl #3]
-	mov 	x13, x2
-	mov 	x14, x1
-	// swap is done
+	str		x2, [x3, x8, lsl #3] // arr[j] = arr[j + gap]
+	str		x1, [x3, x9, lsl #3]
+	mov		x13, x2
+	mov		x14, x1
 	mov		x10, #0 // counter for column swap
 	adr		x2, matrix
 	add		x11, x2, x8, lsl #3
 	add		x12, x2, x9, lsl #3
 swap_loop:
 	adr		x1, n
-	ldr		w1, [x1] // load n
+	ldr		w1, [x1]
 	cmp		x10, x1
 	bge		continue
 	ldr		x13, [x11]
 	ldr		x14, [x12]
-	str 	x13, [x12]
-	str 	x14, [x11]
+	str		x13, [x12]
+	str		x14, [x11]
 	adr		x1, m
-	ldr		w1, [x1] // load m
+	ldr		w1, [x1]
 	add		x11, x11, x1, lsl #3
 	add		x12, x12, x1, lsl #3
 	add		x10, x10, #1
-	b 		swap_loop
+	b		swap_loop
 continue:
 	sub		x8, x8, x4 // j - gap
-	b 		L6
+	b		L6
 end:
 	adr		x0, matrix
 	mov		x0, #0
