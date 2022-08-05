19162269268f0cdae28a91da82805cb85f601b4c
kuchebo.av <kuchebo.av@unix.dozen>
2022-08-05 13:47:28 +0300

    shell is done. workfor bytes

--- assembler/ll2/lab.s
+++ assembler/ll2/lab.s
@@ -2,18 +2,18 @@
 	//	Sorting columns of matrix by min elements
 	//	Shell
 	.data
-	.align	3
+	.align	2
 n:
 	.word	4
 m:
 	.word	6
 matrix:
-	.quad	4, 6, 1, 8, 2, 2
-	.quad	1, 2, 3, 4, 5, 5
-	.quad	0, -7, 3, -1, -1, 5
-	.quad	-11, 33, -6, -7, -10, 5
+	.byte	4, 6, 1, 8, 2, 2
+	.byte	1, 2, 3, 4, 5, 5
+	.byte	0, -7, 3, -1, -1, 5
+	.byte	-11, 33, -6, -7, -10, 5
 maxs:
-	.skip	48
+	.skip	48 // m*8 bytes (work for quad)
 	.text
 	.align	2
 	.global _start
@@ -30,22 +30,22 @@ L0:
 	cmp		x4, x1
 	bge		L3
 	mov		x5, #0
-	lsl		x6, x4, #3
-	ldr		x7, [x2, x6]
-	add		x6, x6, x1, lsl #3
+	lsl		x6, x4, #0
+	ldrsb		x7, [x2, x6]
+	add		x6, x6, x1, lsl #0
 	add		x5, x5, #1
 L1:
 	cmp		x5, x0
 	bge		L2
-	ldr		x8, [x2, x6]
-	add		x6, x6, x1, lsl #3
+	ldrsb		x8, [x2, x6]
+	add		x6, x6, x1, lsl #0
 	add		x5, x5, #1
 	cmp		x7, x8
 	ble		L1
 	mov		x7, x8
 	b		L1
 L2:
-	str		x7, [x3, x4, lsl #3]
+	str		x7, [x3, x4, lsl #0]
 	add		x4, x4, #1
 	b		L0
 L3:
@@ -57,53 +57,53 @@ L4:
 	udiv		x4, x4, x6
 	cmp		x4, #0
 	ble		end
-	mov		x7, x4 // i = gap
-	sub		x7, x7, #1
+	mov		w7, w4 // i = gap
+	sub		w7, w7, #1
 L5:
-	add		x7, x7, #1
+	add		w7, w7, #1
 	adr		x1, m
 	ldr		w1, [x1] // load m
-	cmp		x7, x1
+	cmp		w7, w1
 	bge		L4
-	sub		x8, x7, x4 //j = i - gap
+	sub		w8, w7, w4 //j = i - gap
 L6:
-	cmp		x8, #0
+	cmp		w8, #0
 	blt		L5
-	add		x9, x8, x4 // j + gap
-	ldr		x1, [x3, x8, lsl #3] // arr[j]
-	ldr		x2, [x3, x9, lsl #3] // arr[j+gap]
-	cmp		x1, x2
+	add		w9, w8, w4 // j + gap
+	ldrsb		w1, [x3, x8, lsl #0] // arr[j]
+	ldrsb		w2, [x3, x9, lsl #0] // arr[j+gap]
+	cmp		w1, w2
 .ifdef sort
 	ble		L5
 .endif
 .ifdef rsort
 	bge		L5
 .endif
-	str		x2, [x3, x8, lsl #3] // arr[j] = arr[j + gap]
-	str		x1, [x3, x9, lsl #3]
-	mov		x13, x2
-	mov		x14, x1
+	strb		w2, [x3, x8, lsl #0] // arr[j] = arr[j + gap]
+	strb		w1, [x3, x9, lsl #0]
+	mov		w13, w2
+	mov		w14, w1
 	mov		x10, #0 // counter for column swap
 	adr		x2, matrix
-	add		x11, x2, x8, lsl #3
-	add		x12, x2, x9, lsl #3
+	add		w11, w2, w8, lsl #0
+	add		w12, w2, w9, lsl #0
 swap_loop:
 	adr		x1, n
 	ldr		w1, [x1]
-	cmp		x10, x1
+	cmp		w10, w1
 	bge		continue
-	ldr		x13, [x11]
-	ldr		x14, [x12]
-	str		x13, [x12]
-	str		x14, [x11]
+	ldrsb		w13, [x11]
+	ldrsb		w14, [x12]
+	strb		w13, [x12]
+	strb		w14, [x11]
 	adr		x1, m
 	ldr		w1, [x1]
-	add		x11, x11, x1, lsl #3
-	add		x12, x12, x1, lsl #3
+	add		w11, w11, w1, lsl #0
+	add		w12, w12, w1, lsl #0
 	add		x10, x10, #1
 	b		swap_loop
 continue:
-	sub		x8, x8, x4 // j - gap
+	sub		w8, w8, w4 // j - gap
 	b		L6
 end:
 	adr		x0, matrix
@@ -111,3 +111,4 @@ end:
 	mov		x8, #93
 	svc		#0
 	.size	_start, .-_start
+
