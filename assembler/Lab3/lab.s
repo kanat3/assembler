61f0e47d83dbfce823964026c99d02643e6ce924
kuchebo.av <kuchebo.av@unix.dozen>
2022-08-18 14:57:31 +0300

    final version

--- assembler/Lab3/lab.s
+++ assembler/Lab3/lab.s
@@ -1,11 +1,3 @@
-// правим беск ввод
-// во-первых данные выводятся вдвух места в зависимости от нахождения в буфере
-// значит надо добавить работу смещения в функции work но она попротит все данные (значит каждый используемый регистр надо сохранить)
-// функция смещения считывает всю строку до символа \0
-// если слово у нас на стыке, то буфер для вывода выглядит немного странно. там на месте разрыва находится \0. а это потит работу алгоса
-// надо проверить как в исходном коде выгдлядит буфер (мб я напутала с индексами). и если так и должно быть, то нужно склеить местро разрыва для
-// загрузки в алгос смещения
-// все хочу спать всем привет
         .arch   armv8-a
         .data
 mes_N:
@@ -24,15 +16,7 @@ newline:
         .string "\n"
         .equ    len_nl, .-newline
 N:
-        .skip   3
-str:
-        .skip   1024
-        .align  3
-mes_res:
-        .ascii  "'"
-newstr:
-        .skip   1024
-        .align  3
+        .skip   8
         .text
         .align 2
         .global _start
@@ -74,13 +58,14 @@ _start:
 
         mov     x0, #0
         adr     x1, N
-        mov     x2, #1023
+        mov     x2, len_N
         mov     x8, #63
         svc     #0
-        cmp     x0, #1
-        ble     E1
-        b       E2
+        cmp     x0, #2 // N from 1\n to 9\n
+        beq     E2
+        b       E1 // else cases
 E1:
+        mov     x0, #-3
         bl      writerr
         b       _exit
 E2:
@@ -91,50 +76,50 @@ E2:
         mov     w10, '0'
         sub     w20, w20, w10
         cmp     w20, #0 // N < 0 ->in err
-        ble     _exit
+        ble     E1
         ldr     x0, [sp, #16] // load filename
         bl work
 _exit:
-            mov     x8, #93
-            svc     #0
+        mov     x0, #1
+        adr     x1, newline
+        mov     x2, len_nl
+        mov     x8, #64
+        svc     #0
+        mov     x8, #93
+        svc     #0
 
         .size   _start, .-_start
-
-        //!!!!!!!!!
-
 
     .type   left_offset, %function
 
 left_offset:
 // в х1 получаем буфер
-// в х0 получаем кол-во букв!
-// можем получить слклеенную строку вида <balabla omg'\002'blabla> где 002 код начала полноценного сектора данных
-// может быть такое что кол-во обрабатываемых символов 0, тогда сразу выход (к примеру самое первое слово на стыке. до него пробелы)
-        cmp     x0, #0 // trivial case
+// в х0 получаем кол-во букв
+        cmp     x0, #0
         bne     offset_ok
-        mov     x0, x1 // out buffer
+        mov     x0, x1
         b to_ret
 offset_ok:
-        mov     x17, #0 // кол-во букв в конкретном слове
-        mov     x3, x1 // по этому указателю пишем буквы
-        mov     x4, x3 // указатель на начало нашего слова в буфере
+        mov     x17, #0
+        mov     x3, x1
+        mov     x4, x3
         mov     x14, #0
-        sub     x15, x0, #1// кол-во символов в строке
-        ldrb    w13, [x1, x15, lsl #0] //проверяем последний символ \n или ' '
+        sub     x15, x0, #1
+        ldrb    w13, [x1, x15, lsl #0]
         cmp     w13, '\n'
-        mov     x23, #-3 // запомним что надо заменить конец на ' '
-        strb    wzr, [x1, x15] // обзяательно окончание строки в конце
+        mov     x23, #-3
+        strb    wzr, [x1, x15]
         bne     L0
-        mov     x23, #3 // в конце был \n потом заменим
+        mov     x23, #3
         b       L0
 L0:
-        ldrb    w0, [x1], #1 // По х1 смещаемся как по строчке. в х0 символ
-        cbz     w0, L9 // строка закончилась
+        ldrb    w0, [x1], #1
+        cbz     w0, L9
         cmp     w0, ' '
         beq     L0
         cmp     w0, '\t'
         beq     L0
-        cmp     x4, x3 // сравнили адреса строк
+        cmp     x4, x3
         beq     L1
         mov     w0, ' '
         strb    w0, [x3], #1
@@ -144,11 +129,11 @@ L1:
         ldrb    w20, [x20]
         mov     w22, '0'
         sub     w20, w20, w22
-        sub     x2, x1, #1 //запишем начало слова без пробелов в х2
+        sub     x2, x1, #1
         mov     x17, #0
         b       L2
 L2:
-        ldrb    w0, [x1], #1 //идем по слову до пробела
+        ldrb    w0, [x1], #1
         add     x17, x17, #1
         cbz     w0, L3
         cmp     w0, ' '
@@ -158,34 +143,34 @@ L2:
         cmp     w0, '\t'
         bne     L2
 L3:
-        sub     x17, x17, #1 // размер слова - 1 лежит
+        sub     x17, x17, #1
         mov     w21, #0
 L4:
-        cmp     w21, w20 // в w20 лежит N идем по циклу до w21 = w20 L4 -L6
-        bge     L7 // смещение завершено прыгаем на л7
+        cmp     w21, w20
+        bge     L7
         add     w21, w21, #1
-        mov     x18, x2 // Положили начало слова
-        ldrb    w7, [x18, #0]! //-1 сохраняем первую букву слова
+        mov     x18, x2
+        ldrb    w7, [x18, #0]!
         mov     x14, #0 //x17
 L5:
         cmp     x14, x17 // index < len
         bgt     L6
-        ldrb    w0, [x18, #1]! // смещаемся на след букву
-        strb    w0, [x2, x14, lsl #0] // делаем смещение в регистре х2 (это наш Out)
-        add     x14, x14, #1 // счетчик
-        cmp     x14, x17 // проверяем счетчик и кол-во букв - 1
+        ldrb    w0, [x18, #1]!
+        strb    w0, [x2, x14, lsl #0]
+        add     x14, x14, #1
+        cmp     x14, x17
         bgt     L6
         b       L5
 L6:
         strb    w7, [x2, x17, lsl #0]
         b L4
 L7:
-        add     x17, x17, #1 // кол-во буквы в слове
-        sub     x1, x1, #1 // следующий адрес после конца слова
+        add     x17, x17, #1
+        sub     x1, x1, #1
         mov     x14, #0
 L8:
         cmp     x14, x17
-        bge     L0 // смещение закончено. обрабатываем след. слово
+        bge     L0
         ldrb    w0, [x2, x14, lsl #0]
         strb    w0, [x3], #1
         add     x14, x14, #1
@@ -208,23 +193,19 @@ to_ret:
     .size   left_offset, .-left_offset
 
 
-////
-
     .type   work, %function
-// reserve first 16 bytes for x29 and x30
-// then address of filename, file descriptor and buffer address
     .equ    filename, 16
     .equ    fd, 24
     .equ    correct_result, 32
     .equ    buf, 40
 work:
-    mov     x16, #56 // buf_size = 16 // buffer
+    mov     x16, #56 // buf_size = 16
     sub     sp, sp, x16
     stp     x29, x30, [sp]
     mov     x29, sp
 
-    str     x0, [x29, filename] // store filename on stack
-    str     x1, [x29, N] // store N on stack
+    str     x0, [x29, filename]
+    str     x1, [x29, N]
 // open file
     mov     x1, x0
     mov     x0, #-100
@@ -233,9 +214,9 @@ work:
     svc     #0
 
     cmp     x0, #0
-    bge     0f //
+    bge     0f
     bl      writerr
-    b       4f //
+    b       4f
 
 0:
     str     x0, [x29, fd]
@@ -265,7 +246,7 @@ work:
 // correct the line
     add     x0, x29, buf // buffer as an argument
     ldr     x1, [x29, fd] // and fd
-    bl      correct
+    bl      string_processing
 
 // write data to a file
 // здесь лежит строка с некоторым кол-вом слов для обработки (buf)
@@ -302,7 +283,7 @@ work:
     .size   work, .-work
 
 
-    .type   correct, %function
+    .type   string_processing, %function
     .data
     .equ    buf_addr, 16
     .equ    fd_out, 24
@@ -310,7 +291,7 @@ work:
     .equ    save_x3,40
     .text
     .align      2
-correct:
+string_processing:
     sub     sp, sp, #56
     stp     x29, x30, [sp]
     mov     x29, sp
@@ -319,20 +300,20 @@ correct:
 
     mov     x1, x0 // uncorrected string
     mov     x2, x0 // corrected string
-    mov     x10, #-1 // buffer size counter
-    mov     x19, #0 // another buffer size counter
+    mov     x10, #-1 // counter 2
+    mov     x19, #0 // counter 1
     b       0f
 
-// skip spaces before the first word
+// skip spaces before the word
 
 skip_space:
-    mov     x11, #0 // **
+    mov     x11, #0
 0:
     ldrb    w3, [x1], #1
-    add     x10, x10, #1 //сместились на 1 букву +1
+    add     x10, x10, #1
     add     x19, x19, #1
     cmp     x10, #16
-    bge     string_more_than_buffer // bge
+    bge     string_more_than_buffer
 
     cmp     w3, ' '
     beq     0b
@@ -341,23 +322,21 @@ skip_space:
     cmp     w3, '\n'
     beq     end_of_line
 
-    sub     x1, x1, #1 //пробелов нет поэтому смещаемся на 1 назад к адресу начала слова
+    sub     x1, x1, #1
     sub     x10, x10, #1
     sub     x19, x19, #1
 
-// go to the end of another word and compare last symbol
-// and save the beginning of the word
-    mov     x6, x1 //вх6 начало слова
+    mov     x6, x1
 4:
-    ldrb    w3, [x1], #1 // no first symbol ERR аздесь одну букву пропустили
+    ldrb    w3, [x1], #1
     add     x10, x10, #1
     add     x11, x11, #1
 
     mov     x12, #16
     cmp     w3, '\n'
     beq     5f
-    cmp     x10, x12 // beq
-    beq     string_more_than_buffer // если данные = 15 то 16-м будет \n и прыгнем сюда и получим беск цикл
+    cmp     x10, x12
+    beq     string_more_than_buffer
 
     cmp     w3, ' '
     beq     5f
@@ -365,14 +344,10 @@ skip_space:
     beq     5f
 
     b       4b
-// write this word to the buffer, else go to the next word
+// write this word to the buffer
 5:
-    b     write_word // write full word
-
-write_word:
     mov     x1, x6
 
-// write word
 6:
     ldrb    w3, [x1], #1
     cmp     w3, ' '
@@ -384,7 +359,7 @@ write_word:
 
     strb    w3, [x2], #1
     b       6b
-// add space to the end
+
 // and go to the next
 7:
     mov     w3, ' '
@@ -393,7 +368,6 @@ write_word:
     b       skip_space
 
 string_more_than_buffer:
-// save address of the beggining of the last word
     sub     x10, x1, x11
 // write data to a file
     str     x3, [x29, save_x3]
@@ -419,27 +393,23 @@ string_more_than_buffer:
     strb    w3, [x1], #1
     b       0b
 skip_stx:
-    //add     x12, x12, #1
     sub     x11, x11, #1
     b       0b
 1:
 // read new part of data
     mov     x0, #0
-    // buf addr already in the x1
     mov     x2, #16
     sub     x2, x2, x11 // 16 - len
     mov     x8, #63
     svc     #0
-// init pointers and read data size
+
     ldr     x1, [x29, buf_addr]
     ldr     x2, [x29, buf_addr]
     mov     x10, #-1 // hz
 
-// and go ahead
     b       skip_space
 
 
-// if end of line was reached
 end_of_line:
     ldr     w3, [x2, #-1]!
     cmp     w3, ' '
@@ -461,19 +431,22 @@ all_ok:
     ret
 
 
-    .size   correct, .-correct
+    .size   string_processing, .-string_processing
 
         .type   writerr, %function
 
         .data
     nofile:
-        .string "No such file or directory\n"
+        .string "No such file or directory"
         .equ    nofilelen, .-nofile
     permission:
-        .string "Permission denied\n"
+        .string "Permission denied"
         .equ    permissionlen, .-permission
+    wrong_value:
+        .string "Offset value must be from 1 to 9"
+        .equ    wlen, .-wrong_value
     unknown:
-        .string "Unknown error\n"
+        .string "Unknown error"
         .equ    unknownlen, .-unknown
 
         .text
@@ -484,17 +457,23 @@ writerr:
         bne     0f
         adr     x1, nofile
         mov     x2, nofilelen
-        b       2f
+        b       3f
 0:
         cmp     x0, #-13
         bne     1f
         adr     x1, permission
         mov     x2, permissionlen
-        b       2f
+        b       3f
 1:
+        cmp     x0, #-3
+        bne     2f
+        adr     x1, wrong_value
+        mov     x2, wlen
+        b       3f
+2:
         adr     x1, unknown
         mov     x2, unknownlen
-2:
+3:
         mov     x0, #2
         mov     x8, #64
         svc     #0
